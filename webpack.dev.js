443efd5d380854861b1b48fbb19aeaf33b549604
Cristian Sánchez <contact.cristiansanchez@gmail.com>
2022-11-06 23:11:12 +0100

    ⚡Show favicon: Now favicon shows correctly

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,7 +8,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'public/index.html'
+      template: 'public/index.html',
+      favicon: 'public/favicon.svg'
     })
   ],
   module: {
