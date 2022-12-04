010de4c0a96787023435b891d42ef883fb5c67f5
Cristian Sánchez <contact.cristiansanchez@gmail.com>
2022-12-04 23:17:38 +0100

    ✨ 404 page: Now the 404 page is visible

---
+++ src/pages/NotFound/style.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const Parent = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: 100%;
+`
+
+export const H1 = styled.h1`
+    font-size: 6rem;
+    font-family: 'poppins-semibold'
+`
+
+export const H2 = styled.h2`
+    font-size: 3rem;
+    font-family: 'poppins-medium'
+`
