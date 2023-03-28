76b2902c32d604b42b82676183429e3ddc750267
Cristian Sánchez <contact.cristiansanchez@gmail.com>
2023-03-28 22:10:50 +0200

    [FIX] generateStaticParams override getStaticParams

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,12 +1,13 @@
 import fs from 'fs'
 
-export function getStaticParams() {
+export function generateStaticParams() {
   const files = fs.readdirSync('posts')
 
   const slugs = files.map(filename => filename.replace('.md', ''))
   return slugs.map(slug => {slug})
 }
 
+export const dynamicParams = false // true if you want to generate params dynamically otherwise 404
 
 export default function Blog () {
   return (
