6257940fbaa3f89232836ec04b600c47c7d7ebd7
Cristian Sánchez <contact.cristiansanchez@gmail.com>
2023-05-03 23:03:01 +0200

    [FT] Command K first box

--- components/homepage/CommandK.jsx
+++ components/homepage/CommandK.jsx
@@ -21,11 +21,26 @@ export default function CommandK() {
     return () => handleKeyDown
 
   }, [])
+
+  useEffect(() => {
+    if (show) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = 'unset'
+    }
+  }, [show])
+
   return (
     <>
       {
         show && (
-          <div className={style.background}></div>
+          <div className={style.background} onClick={() => setShow(prev => !prev)}>
+            <div className={style.container} onClick={(e) => e.stopPropagation()}>
+              <div className={style.header}>
+                <input type="text" placeholder="Search" /> 
+              </div>
+            </div>
+          </div>
         ) 
       }
     </>
