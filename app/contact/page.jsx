3356edcee63d05c1b18109ab1d5b21ef739857c9
Cristian Sánchez <contact.cristiansanchez@gmail.com>
2023-05-07 23:28:00 +0200

    [FT] Added contact page responsiveness

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,6 +1,12 @@
 import GridLine from '@/components/global/GridLine'
 import style from './contact.module.css'
 
+//social icons
+import github from '@/public/assets/social/github.png'
+import linkedin from '@/public/assets/social/linkedin.png'
+import twitter from '@/public/assets/social/twitter.png'
+import Image from 'next/image'
+
 export default function Contact() {
   return (
     <div className={style.contactContainer}>
@@ -14,6 +20,11 @@ export default function Contact() {
           <GridLine lineType='vertical' position='topLeft' offset='offsetBottom'/>
           <GridLine lineType='vertical' position='topRight' offset='offsetBottom'/>
         </div>
+      </div>
+      <div className={style.socialImages}>
+        <Image src={github} alt='github' className={style.socialImage}/>
+        <Image src={linkedin} alt='linkedin' className={style.socialImage}/>
+        <Image src={twitter} alt='twitter' className={style.socialImage}/>
       </div>
       <form className={style.form}>
         <input type="email" placeholder="name@acme.com" className={style.input}/>
