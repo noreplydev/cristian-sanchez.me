4a60d0907d03bdd77bef992e915688a97a834f56
Cristian Sánchez <contact.cristiansanchez@gmail.com>
2023-05-07 22:08:59 +0200

    [FT] Added contact UI

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,3 +1,4 @@
+import GridLine from '@/components/global/GridLine'
 import style from './contact.module.css'
 
 export default function Contact() {
@@ -5,10 +6,19 @@ export default function Contact() {
     <div className={style.contactContainer}>
       <div className={style.titleContainer}>
         <h1 className={style.title}>Reach me <span>anytime</span></h1>
+        <div className={style.horizontalLine}>
+          <GridLine lineType='horizontal' position='topLeft'/>
+        </div>
+        <GridLine lineType='horizontal' position='bottomLeft'/>
+        <div className={style.verticalLines}>
+          <GridLine lineType='vertical' position='topLeft' offset='offsetBottom'/>
+          <GridLine lineType='vertical' position='topRight' offset='offsetBottom'/>
+        </div>
       </div>
       <form className={style.form}>
-        <input type="text" placeholder="email" />
-        <textarea placeholder="message" />
+        <input type="email" placeholder="name@acme.com" className={style.input}/>
+        <textarea placeholder="Start your message here" className={style.textarea}/>
+        <button className={style.send}>Send</button>
       </form> 
     </div>
   )
